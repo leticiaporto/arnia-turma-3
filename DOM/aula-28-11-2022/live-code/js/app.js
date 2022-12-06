c8d47ee6d97440d5d882999a31772dedee641188
leticiaporto <leticiaportosoares@gmail.com>
2022-12-05 22:19:07 -0300

    wip: 28/11/2022 exercise solution

--- DOM/aula-28-11-2022/live-code/js/app.js
+++ DOM/aula-28-11-2022/live-code/js/app.js
@@ -1,6 +1,10 @@
 const modal = document.getElementById('modal')
 const form = document.getElementById('subscribe')
 let currentQuestion = null
+let currentIndexQuizQuestion = 0
+let currentQuizQuestion = null
+let quizQuestions = []
+let quizAnswers = []
 
 const openModal = () => {
     modal.style.display = "block"
@@ -25,29 +29,10 @@ window.addEventListener("click", (event) => {
     }
 })
 
-const renderQuestions = (questions) => {
-    const questionsContent = document.getElementById('content')
-    questionsContent.innerHTML = ''
-
-    questions.forEach((question) => {
-        questionsContent.innerHTML = questionsContent.innerHTML + `
-            <div class="card mb-3">
-                <div class="card-body d-flex justify-content-between">
-                    <div>${question.title}</div>
-                    <div class="d-flex">
-                        <button type="button" class="button" onclick="editQuestion(${question.id})"><i class="fa-solid fa-pen-to-square fa-lg"></i></button>
-                        <button type="button" class="button remove-button" onclick="deleteQuestion(${question.id})"><i class="fa-solid fa-trash-can fa-lg"></i></button>
-                    </div>
-                </div>
-            </div>
-        `
-    })
-}
-
 const getQuestions = async () => {
     const apiResponse = await fetch('http://localhost:3000/questions')
     const questions = await apiResponse.json()
-    renderQuestions(questions)
+    return questions
 }
 
 const getQuestion = async (id) => {
@@ -85,6 +70,30 @@ const deleteQuestion = async (id) => {
     getQuestions()
 }
 
+const renderQuestions = (questions) => {
+    const questionsContent = document.getElementById('content')
+    questionsContent.innerHTML = ''
+
+    questions.forEach((question) => {
+        questionsContent.innerHTML = questionsContent.innerHTML + `
+            <div class="card mb-3">
+                <div class="card-body d-flex justify-content-between">
+                    <div>${question.title}</div>
+                    <div class="d-flex">
+                        <button type="button" class="button" onclick="editQuestion(${question.id})"><i class="fa-solid fa-pen-to-square fa-lg"></i></button>
+                        <button type="button" class="button remove-button" onclick="deleteQuestion(${question.id})"><i class="fa-solid fa-trash-can fa-lg"></i></button>
+                    </div>
+                </div>
+            </div>
+        `
+    })
+}
+
+const loadQuestions = async () => {
+    const questions = await getQuestions()
+    renderQuestions(questions)
+}
+
 const saveQuestion = async (question) => {
     if (currentQuestion === null) {
         await createQuestion(question)
@@ -93,7 +102,7 @@ const saveQuestion = async (question) => {
         currentQuestion = null
     }
     closeModal()
-    getQuestions()
+    loadQuestions()
 }
 
 const editQuestion = async (id) => {
@@ -106,36 +115,127 @@ const editQuestion = async (id) => {
     openModal()
 }
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault()
+const renderQuizQuestion = (index) => {
+    const questionsContent = document.getElementById('content-quiz')
+    questionsContent.innerHTML = ''
+    const question = quizQuestions[index]
 
-    const title = form.elements['title'].value
-    const alternative1 = form.elements['alternative1Text'].value
-    const alternative2 = form.elements['alternative2Text'].value
-    const alternative3 = form.elements['alternative3Text'].value
-    const alternative4 = form.elements['alternative4Text'].value
-    const correct = form.elements['correct'].value
+    let questionsHtml = `
+        <div class="question-title mb-4">${question.title}</div>
+        <form id="quiz-form">`
 
-    const question = {
-        title,
-        alternatives: [{
-                text: alternative1,
-                correct: correct === 'alternative1'
-            },
-            {
-                text: alternative2,
-                correct: correct === 'alternative2'
-            },
-            {
-                text: alternative3,
-                correct: correct === 'alternative3'
-            },
-            {
-                text: alternative4,
-                correct: correct === 'alternative4'
-            },
-        ]
+    question.alternatives.forEach((alternative, index) => {
+        questionsHtml = questionsHtml + `
+            <div class="mb-3">
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="alternative" id="alternative${index + 1}" value="${index}" />
+                    <label class="form-check-label" for="alternative${index + 1}">${alternative.text}</label>
+                </div>
+            </div>
+        `
+    })
+    questionsHtml = questionsHtml + `
+            <div class="mt-5 d-flex justify-content-between">`
+
+    if (index > 0) {
+        questionsHtml = questionsHtml + `<button type="button" class="primary-button" onclick="navigate('previous')">Anterior</button>`
+    } else {
+        questionsHtml = questionsHtml + `<div class="simulate-primary-button"></div>`
     }
 
-    saveQuestion(question)
-})
+    questionsHtml = questionsHtml + `<div class="footer-text">Questão ${index + 1}/${quizQuestions.length}</div>`
+
+    if (index + 1 !== quizQuestions.length) {
+        questionsHtml = questionsHtml + `<button type="button" class="primary-button" onclick="navigate('next')">Próximo</button>`
+    } else {
+        questionsHtml = questionsHtml + `<button type="button" class="primary-button" onclick="finish()">Finalizar</button>`
+    }
+
+    `</div></form>`
+
+    questionsContent.innerHTML = questionsHtml
+}
+
+const renderQuizResult = (percentage) => {
+    const questionsContent = document.getElementById('content-quiz')
+    questionsContent.innerHTML = `
+        <div class="quiz-result">
+            <div class="result-icon">
+                <i class="fa-regular fa-circle-check fa-8x"></i>
+            </div>
+            <div class="text">
+                QUIZ Finalizado!
+            </div>
+            <div class="text">
+                Você acertou ${percentage}% das questões
+            </div>
+        </div>
+    `
+}
+
+const loadQuiz = async () => {
+    quizQuestions = await getQuestions()
+    currentQuizQuestion = quizQuestions[currentIndexQuizQuestion]
+    renderQuizQuestion(currentIndexQuizQuestion)
+}
+
+const navigate = async (type) => {
+    const alternative = document.getElementById('quiz-form').elements['alternative'].value
+
+    quizAnswers = quizAnswers.filter((answer) => answer.id !== currentQuizQuestion.id)
+
+    const correctAlternativeIndex = currentQuizQuestion.alternatives.findIndex((alternative) => alternative.correct)
+    quizAnswers.push({
+        questionId: currentQuizQuestion.id,
+        alternative,
+        correct: correctAlternativeIndex === parseInt(alternative)
+    })
+
+    type === 'next' ? currentIndexQuizQuestion++ : currentIndexQuizQuestion--
+
+    renderQuizQuestion(currentIndexQuizQuestion)
+
+    currentQuizQuestion = quizQuestions[currentIndexQuizQuestion]
+}
+
+const finish = () => {
+    const countCorrects = quizAnswers.filter((answer) => answer.correct).length
+    const correctPercentage = (countCorrects * 100) / quizQuestions.length
+    renderQuizResult(correctPercentage.toFixed(2))
+}
+
+if (form) {
+    form.addEventListener('submit', (event) => {
+        event.preventDefault()
+
+        const title = form.elements['title'].value
+        const alternative1 = form.elements['alternative1Text'].value
+        const alternative2 = form.elements['alternative2Text'].value
+        const alternative3 = form.elements['alternative3Text'].value
+        const alternative4 = form.elements['alternative4Text'].value
+        const correct = form.elements['correct'].value
+
+        const question = {
+            title,
+            alternatives: [{
+                    text: alternative1,
+                    correct: correct === 'alternative1'
+                },
+                {
+                    text: alternative2,
+                    correct: correct === 'alternative2'
+                },
+                {
+                    text: alternative3,
+                    correct: correct === 'alternative3'
+                },
+                {
+                    text: alternative4,
+                    correct: correct === 'alternative4'
+                },
+            ]
+        }
+
+        saveQuestion(question)
+    })
+}
